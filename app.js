0a7bcdf557040a145f7a21e5b9f480ac5b946274
nerburglur <michaelgjohnson2222@yahoo.com>
2017-12-08 13:49:42 -0800

    commit

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const express = require('express');
+<<<<<<< HEAD
 
 const app = express();
 
@@ -15,3 +16,36 @@ app.listen(3000, () => {
 });
 
 klhdgsklhg
+=======
+const bodyParser = require('body-parser');
+const cookieParser = require('cookie-parser');
+
+const app = express();
+
+app.use(bodyParser.urlencoded({ extended: false}));
+app.use(cookieParser());
+
+app.set('view engine', 'pug');
+
+const mainRoutes = require('./routes');
+const cardRoutes = require('./routes/cards');
+
+app.use(mainRoutes);
+app.use('/cards', cardRoutes);
+
+app.use((req, res, next) => {
+    const err = new Error('Not Found');
+    err.status = 404;
+    next(err);
+});
+
+app.use((err, req, res, next) => {
+  res.locals.error = err;
+  res.status(err.status);
+  res.render('error');
+});
+
+app.listen(3000, () => {
+    console.log('The application is running on localhost:3000!')
+});
+>>>>>>> commit message
